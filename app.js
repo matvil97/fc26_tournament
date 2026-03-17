b0d0477b44291b4ded6a0811a5d390709f813d41
matvil97 <84513556+matvil97@users.noreply.github.com>
2026-03-17 20:57:54 +0100

    script_update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/TON_DEPLOIEMENT/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzrML9WhDUjPxJ1RbmDHS0tGv95ngLHQ0Th7ywDvbHA5G_vK7zttRIXyyApjgag4VHX/exec";
 
 const form = document.getElementById("registrationForm");
 const payBtn = document.getElementById("payBtn");
